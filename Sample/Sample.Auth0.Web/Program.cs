53e959c75faf6ebf74a871a0d168524ab00c044f
Kevin <whoiskevinrich@gmail.com>
2017-05-24 21:39:26 -0700

    removed angular stuff

--- Sample/Sample.Auth0.Web/Program.cs
+++ Sample/Sample.Auth0.Web/Program.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 
-namespace Sample.Angular
+namespace Sample.Auth0.Web
 {
     public class Program
     {
